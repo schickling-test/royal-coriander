eec9eebb2af9379df5b07e4c83336ab110438769
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 14:47:25 +0000

    [[...slug]].tsx: changed

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -16,7 +16,7 @@ const DocPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ doc
     <div data-sb-object-id={doc._raw.sourceFilePath}>
       <div style={{ display: 'flex', paddingBottom: 10, borderBottom: '1px solid #eee' }}>
         {navInfo.map(({ path, title }) => (
-          <a key={path} href={path} style={{ paddingRight: 6 }} data-sb->
+          <a key={path} href={path} style={{ paddingRight: 6 }} data-sb-field-path='.title'>
             {title}
           </a>
         ))}
